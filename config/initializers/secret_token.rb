a19af955384c2acd49a40c174397cfc715d47a1f
Xingzhong <xu.xeno@gmail.com>
2011-12-15 16:06:00 -0500

    init commit for my first rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstock::Application.config.secret_token = '05dd16fc15af78dd5cf7e9c6a192abc33d444df0d750f2205db3c1674ca85d98c24decda29beb4f5564a413414bd3f9443945b51bec63866591a15dd2f648ae6'
